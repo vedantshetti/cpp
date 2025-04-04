3c9c04643f822d9e75d35cfa523c9c2827bdf268
vedantshetti <vedantshetti123456@gmail.com>
2025-04-04 12:26:57 +0530

    added the reccursive code

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World! Your C++ editor is working correctly." << std::endl;
+    return 0;
+}#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! Your C++ editor is working correctly." << std::endl;
+    return 0;
+}
+
---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int factorial(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+
+    int smallOutput = factorial(n-1);
+    int output = n * smallOutput;
+    return output;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int ans = factorial(n);
+    cout << ans << endl;
+    return 0;
+
+}
